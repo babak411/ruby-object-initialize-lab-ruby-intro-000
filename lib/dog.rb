7c675349f2819eca77d30b5fa795119a4958804d
Babak Khodabandeh <babak.khodabandeh@gmail.com>
2020-04-06 17:33:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed="Mutt")
     @name = name
-    @breed = puts "Mutt"
+    @breed = breed
   end
   def name=(new_name)
     @name = new_name
